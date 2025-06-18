5d78f8e2f2c090513e5931b5d132a49d5451ff0f
namtp01 <tranphuongnam2812@gmail.com>
2025-06-18 07:31:45 +0700

    06: Categories finalization

--- src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -1,8 +1,8 @@
-import { Category } from "@/payload-types";
 import Link from "next/link";
+import { CustomCategory } from "../types";
 
 interface Props {
-    category: Category;
+    category: CustomCategory;
     isOpen: boolean;
     position: { top: number; left: number; }
 }
@@ -24,7 +24,8 @@ export const SubcategoryMenu = ({ category, isOpen, position }: Props) => {
             >
                 <div>
                     {category.subcategories?.map((subcategory: Category) => (
-                        <Link key={subcategory.slug} href="/"
+                        <Link key={subcategory.slug} 
+                            href={`/${category.slug}/${subcategory.slug}`}
                             className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
                         >
                             {subcategory.name}
