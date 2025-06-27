e45cc51f4c4e8b0474b8d70ca3346ac64d5d9977
namtp01 <tranphuongnam2812@gmail.com>
2025-06-27 15:26:43 +0700

    13: api filters sorting

--- src/modules/products/ui/components/product-filters.tsx
+++ src/modules/products/ui/components/product-filters.tsx
@@ -3,8 +3,9 @@
 import { cn } from "@/lib/utils";
 import { ChevronDownIcon, ChevronRightIcon } from "lucide-react";
 import { useState } from "react"
-import { PriceFilter } from "./price-filters";
+import { PriceFilter } from "./price-filter";
 import { useProductFilters } from "../../hooks/use-product-filters";
+import { TagsFilter } from "./tags-filter";
 
 interface Props {
     title: string,
@@ -20,7 +21,7 @@ const ProductFilter = ({ title, className, children }: Props) => {
     return (
         <div className={cn("p-4 border-b flex flex-col gap-2", className)}>
             <div onClick={() => setIsOpen((current) => !current)}
-                className="flex items-center justify-between cursor-pointer"    
+                className="flex items-center justify-between cursor-pointer"
             >
                 <p className="font-medium">{title}</p>
                 <Icon className="size-5" />
@@ -33,24 +34,53 @@ const ProductFilter = ({ title, className, children }: Props) => {
 export const ProductFilters = () => {
     const [filters, setFilters] = useProductFilters();
 
+    const hasAnyFilters = Object.entries(filters).some(([key, value]) => {
+        if (key === "sort") return false;
+
+        if (Array.isArray(value)) {
+            return value.length > 0;
+        }
+
+        if (typeof value === "string") {
+            return value !== "";
+        }
+
+        return value !== null;
+    })
+
+    const onClear = () => {
+        setFilters({
+            minPrice: "",
+            maxPrice: "",
+            tags: [],
+        })
+    }
+
     const onChange = (key: keyof typeof filters, value: unknown) => {
-        setFilters({ ...filters, [key]: value})
+        setFilters({ ...filters, [key]: value })
     };
 
     return (
         <div className="border rounded-md bg-white">
             <div className="p-4 border-b flex items-center justify-between">
                 <p className="font-medium">Filters</p>
-                <button className="underline" onClick={() => {}} type="button">
-                    Clear
-                </button>
+                {hasAnyFilters && (
+                    <button className="underline cursor-pointer" onClick={() => onClear()} type="button">
+                        Clear
+                    </button>
+                )}
             </div>
-            <ProductFilter title="Price" className="border-b-0">
+            <ProductFilter title="Price">
                 <PriceFilter
                     minPrice={filters.minPrice}
                     maxPrice={filters.maxPrice}
                     onMinPriceChange={(value) => onChange("minPrice", value)}
                     onMaxPriceChange={(value) => onChange("maxPrice", value)}
+                />
+            </ProductFilter>
+            <ProductFilter title="Tags" className="border-b-0">
+                <TagsFilter value={filters.tags}
+                    onChange={(value) => onChange("tags", value)}
                 />
             </ProductFilter>
         </div>
