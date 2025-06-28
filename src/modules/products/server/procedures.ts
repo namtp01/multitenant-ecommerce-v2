5c186b66b567e41678531f7db0dda82bc190b31b
namtp01 <tranphuongnam2812@gmail.com>
2025-06-28 19:14:36 +0700

    16: tenant pages

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,4 +1,4 @@
-import { Category, Media } from "@/payload-types";
+import { Category, Media, Tenant } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import type { Sort, Where } from "payload";
 import z from "zod";
@@ -13,7 +13,8 @@ export const productsRouter = createTRPCRouter({
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
         tags: z.array(z.string()).nullable().optional(),
-        sort: z.enum(sortValues).nullable().optional()
+        sort: z.enum(sortValues).nullable().optional(),
+        tenantSlug: z.string().nullable().optional()
     })).query(async ({ ctx, input }) => {
         const where: Where = {};
         let sort: Sort = "-createdAt";
@@ -43,6 +44,12 @@ export const productsRouter = createTRPCRouter({
         } else if (input.maxPrice) {
             where.price = {
                 less_than_equal: input.maxPrice
+            }
+        }
+
+        if (input.tenantSlug) {
+            where["tenant.slug"] = {
+                equals: input.tenantSlug
             }
         }
 
@@ -91,7 +98,7 @@ export const productsRouter = createTRPCRouter({
 
         const data = await ctx.db.find({
             collection: "products",
-            depth: 1, // Populate categories & image
+            depth: 2, // Populate categories & image / tenant & tenant.image
             where,
             sort,
             page: input.cursor,
@@ -103,6 +110,7 @@ export const productsRouter = createTRPCRouter({
             docs: data.docs.map((doc) => ({
                 ...doc,
                 image: doc.image as Media | null,
+                tenant: doc.tenant as Tenant & { image: Media | null }
             }))
         }
     })
--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -1,19 +1,30 @@
+import { generateTenantURL } from "@/lib/utils";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
     id: string;
     name: string;
     imageUrl?: string | null;
-    authorUsername: string;
-    authorImageUrl?: string | null;
+    tenantSlug: string;
+    tenantImageUrl?: string | null;
     reviewRating: number;
     reviewCount: number;
     price: number;
 }
 
-export const ProductCard = ({ id, name, imageUrl, authorUsername, authorImageUrl, reviewRating, reviewCount, price }: ProductCardProps) => {
+export const ProductCard = ({ id, name, imageUrl, tenantSlug, tenantImageUrl, reviewRating, reviewCount, price }: ProductCardProps) => {
+    const router = useRouter()
+    
+    const handleUserClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        router.push(generateTenantURL(tenantSlug))
+    }
+    
     return (
         <div>
             <Link href={`/products/${id}`}>
@@ -28,17 +39,17 @@ export const ProductCard = ({ id, name, imageUrl, authorUsername, authorImageUrl
                     </div>
                     <div className="p-4 border-y flex flex-col gap-3 flex-1">
                         <h2 className="text-lg font-medium line-clamp-4">{name}</h2>
-                        <div className="flex items-center gap-2" onClick={() => {}}>
-                            {authorImageUrl && (
+                        <div className="flex items-center gap-2" onClick={handleUserClick}>
+                            {tenantImageUrl && (
                                 <Image
-                                    alt={authorUsername}
-                                    src={authorImageUrl}
+                                    alt={tenantSlug}
+                                    src={tenantImageUrl}
                                     width={16}
                                     height={16}
                                     className="rounded-full border shrink-0 size-[16px]"
                                 />
                             )}
-                            <p className="text-sm underline font-medium">{authorUsername}</p>
+                            <p className="text-sm underline font-medium">{tenantSlug}</p>
                         </div>
                         {reviewCount > 0 && (
                             <div className="flex items-center gap-1">
